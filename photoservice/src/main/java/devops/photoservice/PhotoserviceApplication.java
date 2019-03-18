710a354a08393a9c663e7412fa1fdbefc99aa2f9
Philip Mc Kenna <44911296+pmckenna25@users.noreply.github.com>
2019-03-18 18:35:20 +0000

    first commit
    Startout spring boot project for photoservice week 9 bv devops academy

---
+++ photoservice/src/main/java/devops/photoservice/PhotoserviceApplication.java
@@ -0,0 +1,14 @@
+package devops.photoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoserviceApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PhotoserviceApplication.class, args);
+	}
+
+}
