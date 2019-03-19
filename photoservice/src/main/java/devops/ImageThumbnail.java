6af6aeab4c40eaeb900b048013590c12e26c8272
Philip Mc Kenna <pmckenna25@hotmail.co.uk>
2019-03-19 13:25:32 +0000

    now a fully working application

--- photoservice/src/main/java/devops/ImageThumbnail.java
+++ photoservice/src/main/java/devops/ImageThumbnail.java
@@ -1,4 +1,4 @@
-package devops.model;
+package devops;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -6,8 +6,6 @@ import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import javax.imageio.ImageIO;
 import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
 public class ImageThumbnail {
@@ -16,14 +14,15 @@ public class ImageThumbnail {
     private AmazonS3 s3;
     private final String bucketName = "bvdevphotobucket";
 
+
     public ImageThumbnail(String image) throws IOException {
-        this.thumbnail = retrieveImage(image);
         s3 = AmazonS3ClientBuilder.defaultClient();
+        this.thumbnail = retrieveImage(image);
     }
 
     private Image retrieveImage(String image) throws IOException {
 
-        S3Object s3Object = s3.getObject(new GetObjectRequest(bucketName, image));
+        S3Object s3Object = s3.getObject(bucketName, image);
         Image inputImage = ImageIO.read(s3Object.getObjectContent());
         inputImage = inputImage.getScaledInstance(100,100, Image.SCALE_DEFAULT);
         return inputImage;
--- photoservice/src/main/java/devops/ecController.java
+++ photoservice/src/main/java/devops/ecController.java
@@ -1,18 +1,13 @@
-package devops.controller;
+package devops;
 
-import devops.model.ImageThumbnail;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -31,10 +26,21 @@ public class ecController {
 
         ImageThumbnail resizeImage = new ImageThumbnail(image);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write((BufferedImage) resizeImage.getThumbnail(), "jpg", outputStream);
+        ImageIO.write(convertToBufferedImage(resizeImage.getThumbnail()), "png", outputStream);
         return ResponseEntity
                 .ok()
                 .contentType(MediaType.IMAGE_JPEG)
                 .body(outputStream.toByteArray());
+    }
+
+    public static BufferedImage convertToBufferedImage(Image image)
+    {
+        BufferedImage newImage = new BufferedImage(
+                image.getWidth(null), image.getHeight(null),
+                BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = newImage.createGraphics();
+        g.drawImage(image, 0, 0, null);
+        g.dispose();
+        return newImage;
     }
 }
